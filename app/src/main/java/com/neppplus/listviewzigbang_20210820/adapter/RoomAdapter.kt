70f42361040f98334f00971ce4fe15136e75bfc6
이재환 <qorvhfl080@naver.com>
2021-08-20 17:36:38 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/listviewzigbang_20210820/adapter/RoomAdapter.kt
@@ -0,0 +1,41 @@
+package com.neppplus.listviewzigbang_20210820.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.neppplus.listviewzigbang_20210820.R
+import com.neppplus.listviewzigbang_20210820.data.Room
+
+class RoomAdapter(val mContext: Context, val resId: Int, val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        val roomImg = row.findViewById<ImageView>(R.id.roomImg)
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        roomImg.setImageResource(data.img)
+        priceTxt.text = "${data.price}"
+        addressTxt.text = data.address
+        descriptionTxt.text = data.description
+
+        return row
+    }
+
+}
---
+++ app/src/main/java/com/neppplus/listviewzigbang_20210820/data/Room.kt
@@ -0,0 +1,4 @@
+package com.neppplus.listviewzigbang_20210820.data
+
+class Room(val img: Int, val price: Int, val address: String, val description: String) {
+}
