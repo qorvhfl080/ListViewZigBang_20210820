9201a44d777588f531958502ad41a43889726132
이재환 <qorvhfl080@naver.com>
2021-08-23 11:59:23 +0900

    앱의 시작화면 - 스플래시 액티비티

---
+++ app/src/main/java/com/neppplus/listviewzigbang_20210820/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listviewzigbang_20210820
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
