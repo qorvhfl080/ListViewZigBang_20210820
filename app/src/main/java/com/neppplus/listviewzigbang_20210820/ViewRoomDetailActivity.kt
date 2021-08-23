f87dd8cfaa4558520bedf9c651af3ba742f783ca
이재환 <qorvhfl080@naver.com>
2021-08-23 10:42:43 +0900

    방 상세 화면 추가중...

---
+++ app/src/main/java/com/neppplus/listviewzigbang_20210820/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listviewzigbang_20210820
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
