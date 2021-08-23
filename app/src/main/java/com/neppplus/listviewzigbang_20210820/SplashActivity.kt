a2cd4eeb0627ec2d5e04133e114be3e635245e43
이재환 <qorvhfl080@naver.com>
2021-08-23 12:19:13 +0900

    postDelayed 활용하여 로딩화면 3초 보여주기

--- app/src/main/java/com/neppplus/listviewzigbang_20210820/SplashActivity.kt
+++ app/src/main/java/com/neppplus/listviewzigbang_20210820/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.neppplus.listviewzigbang_20210820
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        로딩 화면 -> 3초후 메인화면으로 이동
+
+//        인텐트 코드를 3초 후에 실행
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000)
+
+
     }
 }
